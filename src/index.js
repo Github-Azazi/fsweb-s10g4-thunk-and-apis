dd620b926a4e3cf688d4ba9fc344feaae2f021cf
Github-Azazi <cs50_scratch@yahoo.com>
2024-03-18 11:05:22 +0300

    S10G4 its done

--- src/index.js
+++ src/index.js
@@ -1,16 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
-import { createStore } from "redux";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "react-toastify/dist/ReactToastify.css";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter } from "react-router-dom";
+import { legacy_createStore as createStore } from "redux";
 import { Provider } from "react-redux";
-import { myReducer } from './reducers';
+import { myReducer } from "./reducers";
+import {thunk} from "redux-thunk";
+import logger from "redux-logger";
 
-const depo = createStore(myReducer);
+import { applyMiddleware, combineReducers } from "redux";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const reducers = combineReducers({
+  catFacts: myReducer,
+});
+
+const middlewares = applyMiddleware(thunk, logger);
+
+const depo = createStore(reducers, middlewares);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={depo}>
     <BrowserRouter>
--- src/reducers.js
+++ src/reducers.js
@@ -1,3 +1,5 @@
+import { nanoid } from "nanoid";
+
 import {
   FAV_ADD,
   FAV_REMOVE,
@@ -7,11 +9,13 @@ import {
   GET_FAVS_FROM_LS,
 } from "./actions";
 
+import { toast } from "react-toastify";
+
 const initial = {
   favs: [],
-  current: null,
-  error: null,
-  loading: true,
+  current: "",
+  error: "",
+  loading: false,
 };
 
 function writeFavsToLocalStorage(state) {
@@ -25,22 +29,43 @@ function readFavsFromLocalStorage() {
 export function myReducer(state = initial, action) {
   switch (action.type) {
     case FAV_ADD:
-      return state;
+      for (const iterator of state.favs) {
+        if (iterator.fact === action.payload) return state;
+      }
+
+      toast.info("Favorilere eklendi");
+      const stateAfterAdd = {
+        ...state,
+        favs: [...state.favs, { id: nanoid(), fact: action.payload }],
+      };
+      writeFavsToLocalStorage(stateAfterAdd);
+      return stateAfterAdd;
 
     case FAV_REMOVE:
-      return state;
+      toast.info("Favorilerden çıkartıldı");
+      const stateAfterRemove = {
+        ...state,
+        favs: state.favs.filter((fav) => fav.id !== action.payload),
+      };
+      writeFavsToLocalStorage(stateAfterRemove);
+      return stateAfterRemove;
 
     case FETCH_SUCCESS:
-      return state;
+      toast.success("Kedi Fact'iniz hazır🐈");
+      return { ...state, loading: false, current: action.payload };
 
     case FETCH_LOADING:
-      return state;
+      return { ...state, loading: true, error: "" };
 
     case FETCH_ERROR:
-      return state;
+      toast.error("Kedi Fact'inizi getiremedik😿");
+      return { ...state, loading: false, error: action.payload };
 
     case GET_FAVS_FROM_LS:
-      return state;
+      const lsFavs = readFavsFromLocalStorage();
+      if (!lsFavs) return state;
+
+      return { ...state, favs: readFavsFromLocalStorage() };
 
     default:
       return state;
